18d3c16f25214fbc43779851ee41d5ef81bfaaf0
Diego Ortiz <diegoortizmatajira@gmail.com>
2026-01-08 11:36:52 -0500

    WIP

--- src/quick_llm/chain_factory.py
+++ src/quick_llm/chain_factory.py
@@ -1,46 +1,72 @@
 """Factory class for managing language model instances."""
 
 import logging
-from typing import Callable, Generic, Self
+from typing import Callable, Generic, Self, cast, overload
 
 from langchain_core.language_models import (
     BaseLanguageModel,
+    LanguageModelLike,
+    LanguageModelOutput,
 )
 from langchain_core.language_models.base import LanguageModelOutputVar
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from langchain_core.prompt_values import PromptValue
 from langchain_core.prompts.base import BasePromptTemplate
-from langchain_core.runnables import RunnableGenerator
+from langchain_core.prompts.prompt import PromptTemplate
+from langchain_core.prompts.string import PromptTemplateFormat
+from langchain_core.runnables import RunnableGenerator, RunnableLambda
 from langchain_core.runnables.base import RunnableSerializable
+from pydantic import BaseModel
 
 from quick_llm.prompt_input_parser import PromptInputParser
-from quick_llm.type_definitions import ChainInputType, ChainOutputVar, PromptOutputVar
+from quick_llm.type_definitions import ChainInputType, ChainOutputVar
 
 
-class ChainFactory(
-    Generic[
-        PromptOutputVar,
-        LanguageModelOutputVar,
-        ChainOutputVar,
-    ]
-):
+class ChainFactory(Generic[ChainOutputVar]):
     """Factory class for managing language model instances."""
 
-    def __init__(self):
+    def __init__(
+        self,
+        output_type: type[ChainOutputVar] = str,
+    ) -> None:
         self.__logger = logging.getLogger(__name__)
         self.__input_transformer: RunnableGenerator[ChainInputType, dict] | None = None
         self.__output_transformer: (
-            RunnableGenerator[LanguageModelOutputVar, ChainOutputVar] | None
+            RunnableSerializable[LanguageModelOutput, ChainOutputVar] | None
         ) = None
-        self.__language_model: BaseLanguageModel[LanguageModelOutputVar] | None = None
-        self.__prompt_template: BasePromptTemplate[PromptOutputVar] | None = None
+        self.__language_model: LanguageModelLike | None = None
+        self.__prompt_template: BasePromptTemplate[PromptValue] | None = None
         self.__param_input: str = "input"
         self.__param_format_instructions: str = "format_instructions"
+        self.__json_model: type[BaseModel] | None = None
+        # Conditional initialization based on output_type
+        self.__output_type = output_type
+        if self.__output_type is str:
+            self.use_output_transformer(
+                cast(
+                    RunnableSerializable[LanguageModelOutput, ChainOutputVar],
+                    StrOutputParser(),
+                )
+            )
+
+    @staticmethod
+    def for_json_model(
+        json_model: type[BaseModel],
+    ) -> "ChainFactory[dict[str, object]]":
+        """
+        Creates a ChainFactory instance based on a given JSON model.
+
+        :param json_model: A Pydantic BaseModel class that will be used to interpret JSON outputs.
+        :return: A ChainFactory instance configured to use the provided JSON model.
+        """
+        return ChainFactory(dict[str, object]).use_json_model(json_model)
 
     def __fail(self, message: str) -> Exception:
         self.__logger.error(message)
-        return ValueError(message)
+        return RuntimeError(message)
 
     @property
-    def language_model(self) -> BaseLanguageModel[LanguageModelOutputVar]:
+    def language_model(self) -> LanguageModelLike:
         """
         Gets the language model instance.
 
@@ -51,7 +77,7 @@ class ChainFactory(
         return self.__language_model
 
     @property
-    def prompt_template(self) -> BasePromptTemplate[PromptOutputVar]:
+    def prompt_template(self) -> BasePromptTemplate[PromptValue]:
         """
         Gets the prompt template instance.
 
@@ -60,6 +86,24 @@ class ChainFactory(
         if self.__prompt_template is None:
             raise self.__fail("Prompt template is not set.")
         return self.__prompt_template
+
+    @property
+    def input_param(self) -> str:
+        """
+        Gets the name of the input parameter.
+
+        :return: The name of the input parameter.
+        """
+        return self.__param_input
+
+    @property
+    def format_instructions_param(self) -> str:
+        """
+        Gets the name of the format instructions parameter.
+
+        :return: The name of the format instructions parameter.
+        """
+        return self.__param_format_instructions
 
     @property
     def input_transformer(self) -> RunnableGenerator[ChainInputType, dict]:
@@ -73,9 +117,38 @@ class ChainFactory(
         return self.__input_transformer
 
     @property
+    def additional_values_injector(self) -> RunnableLambda[dict, dict]:
+        """
+        Provides a lambda function that injects additional values into the existing input dictionary.
+
+        This method creates a dictionary of additional values to be passed into the chain. If the JSON model
+        is being used and the output transformer is of the type JsonOutputParser, it adds format instructions
+        specific to the JSON model to the `additional_values` dictionary. The lambda function merges the
+        existing input dictionary with these additional values.
+
+        :return: A RunnableLambda instance that injects additional values into the input dictionary.
+        """
+        additional_values: dict[str, object] = {}
+
+        output_transformer = self.output_transformer
+
+        if self.__json_model and isinstance(output_transformer, JsonOutputParser):
+            # Adds format instructions for JSON model if applicable
+            self.__logger.debug("Building chain with JSON model: %s", self.__json_model)
+            additional_values[self.format_instructions_param] = (
+                output_transformer.get_format_instructions()
+            )
+            self.__logger.debug(
+                "Added format instructions to chain: %s", additional_values
+            )
+
+        # Returns an injector for additional values
+        return RunnableLambda[dict, dict](lambda x: {**x, **additional_values})
+
+    @property
     def output_transformer(
         self,
-    ) -> RunnableGenerator[LanguageModelOutputVar, ChainOutputVar]:
+    ) -> RunnableSerializable[LanguageModelOutputVar, ChainOutputVar]:
         """
         Gets the output transformer instance.
 
@@ -133,8 +206,9 @@ class ChainFactory(
         )
         return self
 
+    @overload
     def use_prompt_template(
-        self, prompt_template: BasePromptTemplate[PromptOutputVar]
+        self, prompt_template: BasePromptTemplate[PromptValue]
     ) -> Self:
         """
         Sets the prompt template instance.
@@ -142,11 +216,68 @@ class ChainFactory(
         :param prompt_template: An instance of PromptTemplate to set.
         :return: The ChainFactory instance for method chaining.
         """
+
+    @overload
+    def use_prompt_template(
+        self,
+        prompt_template: str,
+        prompt_template_format: PromptTemplateFormat = "f-string",
+        partial_variables: dict[str, str] | None = None,
+    ) -> Self:
+        """
+        Sets the prompt template instance from a string.
+
+        :param prompt_template: A string representing the prompt template.
+        :param prompt_template_format: The format of the prompt template string.
+        :param partial_variables: A dictionary of partial variables for the prompt template.
+        :return: The ChainFactory instance for method chaining.
+        """
+
+    def use_prompt_template(
+        self,
+        prompt_template: str | BasePromptTemplate[PromptValue],
+        prompt_template_format: PromptTemplateFormat = "f-string",
+        partial_variables: dict[str, str] | None = None,
+    ) -> Self:
+        """
+        Sets the prompt template instance.
+        :param prompt_template: An instance of PromptTemplate or a string representing
+        the prompt template.
+        :param prompt_template_format: The format of the prompt template string.
+        :param partial_variables: A dictionary of partial variables for the prompt template.
+        :return: The ChainFactory instance for method chaining.
+        """
+        if isinstance(prompt_template, str):
+            # Creates a PromptTemplate from string
+            prompt_template = PromptTemplate.from_template(
+                template=prompt_template,
+                template_format=prompt_template_format,
+                partial_variables=partial_variables,
+            )
         self.__prompt_template = prompt_template
         return self
 
+    def use_json_model(self, model: type[BaseModel]) -> Self:
+        """
+        Sets the JSON model for output parsing.
+
+        :param model: A Pydantic BaseModel class to parse the output into.
+        :return: The ChainFactory instance for method chaining.
+        """
+        self.__json_model = model
+        self.use_output_transformer(
+            cast(
+                RunnableSerializable[LanguageModelOutput, ChainOutputVar],
+                JsonOutputParser(pydantic_object=self.__json_model),
+            )
+        )
+        self.__logger.debug(
+            "Setting JSON model for output parsing: %s", self.__json_model
+        )
+        return self
+
     def use_output_transformer(
-        self, output_parser: RunnableGenerator[LanguageModelOutputVar, ChainOutputVar]
+        self, output_parser: RunnableSerializable[LanguageModelOutput, ChainOutputVar]
     ) -> Self:
         """
         Sets the output transformer instance.
@@ -159,11 +290,24 @@ class ChainFactory(
         self.__logger.debug("Setting output transformer: %s", self.__output_transformer)
         return self
 
-    def build_raw_chain(
+    def build(
         self,
     ) -> RunnableSerializable[ChainInputType, ChainOutputVar]:
+        """
+        Constructs and returns the complete runnable chain.
+
+        The chain consists of the following components, connected sequentially:
+        - Input transformer: Transforms raw input into a structured format.
+        - Additional values injector: Injects additional parameters required for the chain.
+        - Prompt template: Generates the prompt based on the transformed input.
+        - Language model: Generates an output based on the prompt.
+        - Output transformer: Parses and transforms the model output into the desired format.
+
+        :return: A RunnableSerializable instance representing the complete chain.
+        """
         chain = (
             self.input_transformer
+            | self.additional_values_injector
             | self.prompt_template
             | self.language_model
             | self.output_transformer
--- tests/chain_factory_test.py
+++ tests/chain_factory_test.py
@@ -1,25 +1,99 @@
-from langchain_core.language_models import FakeListChatModel, FakeListLLM
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts.prompt import PromptTemplate
+"""Tests for the ChainFactory class"""
+
+import pytest
+from langchain_core.language_models import (
+    BaseLanguageModel,
+    FakeListChatModel,
+    FakeListLLM,
+)
+from pydantic import BaseModel, Field
 
 from quick_llm.chain_factory import ChainFactory
+from quick_llm.type_definitions import ChainInputType
 
 TEST_INPUT = "Test input"
 TEST_EXPECTED_RESPONSE = "This is a sample response."
+TEST_INPUT_SAMPLES: list[ChainInputType] = [
+    {"input": TEST_INPUT},
+    TEST_INPUT,
+]
 
 
-def test_chain_factory():
-    models = [
-        FakeListLLM(responses=[TEST_EXPECTED_RESPONSE]),
-        FakeListChatModel(responses=[TEST_EXPECTED_RESPONSE]),
+class AnswerOutput(BaseModel):
+    """Sample object structure to test the JSON parsing feature"""
+
+    what: str = Field(description="Summarizes/rephrase the question being answered.")
+    when: str = Field(
+        description="Provides a date-formatted answer to the question when required."
+    )
+    who: str = Field(
+        description="Provides a proper name answer to the question when required."
+    )
+    general: str = Field(description="Provides a short-text answer to the question.")
+
+
+def __get_test_models(expected_response: str) -> list[BaseLanguageModel]:
+    return [
+        FakeListLLM(responses=[expected_response]),
+        FakeListChatModel(responses=[expected_response]),
     ]
+
+
+@pytest.mark.parametrize("input_value", TEST_INPUT_SAMPLES)
+def test_string_chain_factory(input_value: ChainInputType):
+    """Test the factory with a string output"""
+    models = __get_test_models(TEST_EXPECTED_RESPONSE)
     for model in models:
         factory = (
             ChainFactory()
-            .use_prompt_template(PromptTemplate.from_template("Sample Prompt {input}"))
+            .use_prompt_template("Sample Prompt {input}")
             .use_language_model(model)
-            .use_output_transformer(StrOutputParser())
         )
-        chain = factory.build_raw_chain()
-        response = chain.invoke(TEST_INPUT)
+        chain = factory.build()
+        response = chain.invoke(input_value)
         assert response == TEST_EXPECTED_RESPONSE
+
+
+@pytest.mark.parametrize("input_value", TEST_INPUT_SAMPLES)
+def test_json_chain_factory(input_value: ChainInputType):
+    """Test the factory with a json output"""
+    mocked_response = """{"what": "something", "when":"tomorrow", "who": "someone", "general": "something else"}"""
+    models = __get_test_models(mocked_response)
+    for model in models:
+        factory = (
+            ChainFactory.for_json_model(AnswerOutput)
+            .use_prompt_template("Sample Prompt {input}")
+            .use_language_model(model)
+        )
+        chain = factory.build()
+        response = chain.invoke(input_value)
+        # Checks that the response is a populated dictionary
+        assert isinstance(response, dict)
+        assert response.get("what", "nothing") == "something"
+
+
+@pytest.mark.parametrize("input_value", TEST_INPUT_SAMPLES)
+def test_input_transformer(input_value: ChainInputType):
+    """Tests the input parser to ensure it accepts various input formats"""
+    factory = ChainFactory()
+    transformer = factory.input_transformer
+    output = transformer.invoke(input_value)
+    assert output is not None
+    assert output.get(factory.input_param, "empty") == TEST_INPUT
+
+
+def test_additional_values_injector():
+    """Test that additional values are correctly injected for the JSON model.
+
+    Verifies that the ChainFactory's additional_values_injector adds the necessary
+    fields to the input, preserves the original input, and injects specific fields
+    required for a JSON model.
+    """
+    factory = ChainFactory.for_json_model(AnswerOutput)
+    injector = factory.additional_values_injector
+    result = injector.invoke({factory.input_param: TEST_INPUT})
+    assert result is not None
+    # Check that the original input is preserved
+    assert result.get(factory.input_param, "empty") == TEST_INPUT
+    # As the factory is for a JSON model, format_instructions should be injected
+    assert result.get(factory.format_instructions_param, "empty") != "empty"
